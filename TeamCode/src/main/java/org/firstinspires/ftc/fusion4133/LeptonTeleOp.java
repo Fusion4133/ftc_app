bc1f11404f3fce6eb41a51570c37c0ba3498e93e
Fusion4133 <4133.fusion@gmail.com>
2017-02-17 16:40:13 -0700

    2/17/2017- Teleop code

--- TeamCode/src/main/java/org/firstinspires/ftc/fusion4133/LeptonTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/fusion4133/LeptonTeleOp.java
@@ -22,13 +22,33 @@ public class LeptonTeleOp extends OpMode{
     double bpInc = 0.001;
     double tuskINC = 0.002;
     double popperPower = 0.8;
+    String collectionState;
+    boolean triggerPressed;
+    boolean reversedTriggerPressed;
+    DcMotor leftDriveFront;
+    DcMotor leftDriveBack;
+    DcMotor rightDriveFront;
+    DcMotor rightDriveBack;
+    double dirADJ;
 
     @Override
     public void init() {
         //telemetry.addData("Step", "Initializing");
         //telemetry.update();
 
+        collectionState = "off";
+
+        triggerPressed = false;
+
+        reversedTriggerPressed = false;
+
+        dirADJ = -1.0;
+
         robot.init(hardwareMap);
+        leftDriveFront = robot.leftMotorFront;
+        leftDriveBack = robot.leftMotorBack;
+        rightDriveFront = robot.rightMotorFront;
+        rightDriveBack = robot.rightMotorBack;
         robot.leftMotorBack.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         robot.leftMotorFront.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         robot.rightMotorBack.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
@@ -48,23 +68,56 @@ public class LeptonTeleOp extends OpMode{
 
         //JOYSTICK 1
         // This made sure that are two motors are going the same speed
-        leftPower  = -gamepad1.left_stick_y;
-        rightPower = -gamepad1.right_stick_y;
-        robot.leftMotorFront.setPower(leftPower);
-        robot.leftMotorBack.setPower(leftPower);
-        robot.rightMotorFront.setPower(rightPower);
-        robot.rightMotorBack.setPower(rightPower);
+        leftPower  = gamepad1.left_stick_y * dirADJ;
+        rightPower = gamepad1.right_stick_y * dirADJ;
+        leftDriveFront.setPower(leftPower);
+        leftDriveBack.setPower(leftPower);
+        rightDriveFront.setPower(rightPower);
+        rightDriveBack.setPower(rightPower);
+
+        if (gamepad1.a){
+            leftDriveFront = robot.rightMotorBack;
+            leftDriveBack = robot.rightMotorFront;
+            rightDriveFront = robot.leftMotorBack;
+            rightDriveBack = robot.leftMotorFront;
+
+             dirADJ = 1.0;
+
+        }
+        else if (gamepad1.b){
+            leftDriveFront = robot.leftMotorFront;
+            leftDriveBack = robot.leftMotorBack;
+            rightDriveFront = robot.rightMotorFront;
+            rightDriveBack = robot.rightMotorBack;
+
+            dirADJ = -1.0;
+
+        }
 
         //Run collection system with triggers
-        if(gamepad1.right_trigger > 0.3){
-            robot.collectionMotor.setPower(1.0);
+        if(gamepad1.right_trigger > 0.3 && !triggerPressed){
+            if (collectionState.equals("forward")){
+                collectionState = "off";
+                robot.collectionMotor.setPower(0.0);
+            }
+            else {
+                collectionState = "forward";
+                robot.collectionMotor.setPower(1.0);
+            }
         }
-        else if (gamepad1.left_trigger > 0.3){
-            robot.collectionMotor.setPower(-1.0);
+        triggerPressed = gamepad1.right_trigger > 0.3;
+
+        if(gamepad1.left_trigger > 0.3 && !reversedTriggerPressed){
+            if (collectionState.equals("backward")){
+                collectionState = "off";
+                robot.collectionMotor.setPower(0.0);
+            }
+            else {
+                collectionState = "backward";
+                robot.collectionMotor.setPower(-1.0);
+            }
         }
-        else {
-            robot.collectionMotor.setPower(0.0);
-        }
+        reversedTriggerPressed = gamepad1.left_trigger > 0.3;
 
        //JOYSTICK 2
         //Run lift with joy stick
@@ -86,22 +139,25 @@ public class LeptonTeleOp extends OpMode{
         }
 
 
-        //tusks
+        //tusks go up when up on d-pad is pressed.
         if (gamepad2.dpad_up) {
             robot.tuskServo.setPosition(robot.TUSK_UP);
         }
+        //tusk go down when in down on d-pad is pressed.
         else if (gamepad2.dpad_down) {
             robot.tuskServo.setPosition(robot.TUSK_DOWN);
         }
+        //tusk goes to the grabing position for the ball.
         else if (gamepad2.dpad_left) {
             robot.tuskServo.setPosition(robot.TUSK_GRAB);
         }
+        //this is the initial tusk position.
         else if (gamepad2.dpad_right) {
             robot.tuskServo.setPosition(robot.TUSK_READY);
         }
         //telemetry.addData("tuskServo", robot.tuskServo.getPosition());
 
-        //horn
+        //horn servo is not in use.
         if (gamepad2.left_bumper) {
             robot.hornServo.setPosition(robot.HORN_RELEASED);
         }
